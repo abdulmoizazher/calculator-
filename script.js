ed833f0d23ebd1e3ff3df39a753571a0dc0a5c4b
abdulmoizazher <abdulmoizahzer@gmail.com>
2024-08-05 22:55:19 +0500

    monday

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-const isequal = document.querySelector(".isequal")
-const  operator = document.querySelectorAll(".operator")
-const operation = document.querySelectorAll(".operation")
+const isequal = document.querySelector("#isequal")
 const  screen = document.querySelector("#screen")
 const  AC = document.getElementById("AC")
 const root_button = document.getElementById("root")
+const square_button = document.getElementById("square")
 
-function operators ( dom){
-
-    let number = dom.innerHTML
-    return number;
-}
 
 function print (content){
     console.log(content)
@@ -31,9 +25,35 @@ function clear (){
 
 function  root (){
     let value = screen.innerHTML;
-    value = value,isdddsfsdfafdd
-    print((value**1/2))
-}
+    clear();
+    value_int = parseInt(value);
+    if (value_int >0)
+    {
+        print(Math.sqrt(value_int));
+    }
+
+    else {
+        //pass
+    }
+    }
 
 AC.addEventListener("click",clear)
 root_button.addEventListener("click",root)
+
+
+ square = () => {
+
+     let value = screen.innerHTML;
+     clear();
+     if (value > 0) {
+         print(value ** 2);
+     }
+
+     else{
+         //pass
+     }
+
+}
+
+square_button.addEventListener("click",square)
+isequal.addEventListener("click",evals)
